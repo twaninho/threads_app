76a6e72ea8189f25085cfa6a31a72f05c1b18ebf
Twan Hopmans <twanjhopmans@hotmail.com>
2023-08-07 18:35:03 +0200

    Bottombar component

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -1,8 +1,39 @@
+"use client"
+
+import { sidebarLinks } from '@/constants'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
+import Image from 'next/image'
 
 const Bottombar = () => {
+  const pathname = usePathname()
+  
   return (
-    <h1>Bottombar</h1>
+    <section className='bottombar'>
+      <div className='bottombar_container'>
+        {sidebarLinks.map((link) => {
+          const isActive = (pathname.includes(link.route)
+          && link.route.length > 1) || pathname === link.route
+          
+          return (
+            <Link
+            href={link.route}
+            key={link.label}
+            className={`bottombar_link ${isActive && 'bg-primary-500'}`}
+            >
+              <Image 
+              src={link.imgURL}
+              alt={link.label}
+              width={24}
+              height={24}
+              />
+              <p className='text-light-1 text-subtile-medium 
+              max-sm:hidden'>{link.label.split(/\s+/)[0]}</p>
+            </Link>
+          )})}
+      </div>
+    </section>
   )
 }
 
