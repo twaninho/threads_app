f2c7db74fbdc3d803bdd013b73b106da1cdf0674
Twan Hopmans <twanjhopmans@hotmail.com>
2023-08-07 18:44:17 +0200

    rightsidebar component + clerk dark theme

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,6 +1,7 @@
 import { OrganizationSwitcher, SignedIn, SignOutButton } from "@clerk/nextjs";
 import Link from "next/link";
 import Image from "next/image";
+import { dark } from '@clerk/themes'
 
 function Topbar() {
   return (
@@ -28,6 +29,7 @@ function Topbar() {
 
         <OrganizationSwitcher
         appearance={{
+          baseTheme: dark,
           elements: {
             organizationSwitcherTrigger: 'py-6 px-4'
           }
