3976bdae250122d7453a4749bb0fad9a57d07133
Twan Hopmans <twanjhopmans@hotmail.com>
2023-08-07 16:36:22 +0200

    adding images and making topbar component

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 const LeftSidebar = () => {
   return (
-    <div>
-      Leftsidebar
-    </div>
+    <section className='custom-scrollbar leftsidebar'>
+        <div className='flex w-full flex-1 flex-col
+        gap-6 px-6'>
+          
+        </div>
+    </section>
   )
 }
 
