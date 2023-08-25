ca18a459006bc9967d4fcecdf5e674890cbc0afa
Twan Hopmans <twanjhopmans@hotmail.com>
2023-08-25 15:02:30 +0200

    reply/comment

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -66,3 +66,86 @@ export async function fetchPosts(pageNumber = 1, pageSize = 20) {
 
     return { posts, isNext}
 }
+
+export async function fetchThreadById(threadId: string) {
+  connectToDB();
+
+  try {
+    const thread = await Thread.findById(threadId)
+      .populate({
+        path: "author",
+        model: User,
+        select: "_id id name image",
+      }) // Populate the author field with _id and username
+    //   .populate({
+    //     path: "community",
+    //     model: Community,
+    //     select: "_id id name image",
+    //   }) // Populate the community field with _id and name
+      .populate({
+        path: "children", // Populate the children field
+        populate: [
+          {
+            path: "author", // Populate the author field within children
+            model: User,
+            select: "_id id name parentId image", // Select only _id and username fields of the author
+          },
+          {
+            path: "children", // Populate the children field within children
+            model: Thread, // The model of the nested children (assuming it's the same "Thread" model)
+            populate: {
+              path: "author", // Populate the author field within nested children
+              model: User,
+              select: "_id id name parentId image", // Select only _id and username fields of the author
+            },
+          },
+        ],
+      })
+      .exec();
+
+    return thread;
+  } catch (error: any) {
+    console.error(`Error while fetching thread:", ${error.message}`)
+    
+  }
+}
+
+export async function addCommentToThread(
+  threadId: string,
+  commentText: string,
+  userId: string,
+  path: string,
+  ) {
+  connectToDB();
+
+  try {
+    // Find the original thread by its ID
+    const originalThread = await Thread.findById(threadId)
+
+    if(!originalThread) {
+      throw new Error("Thread not found")
+    }
+
+    // Create a new thread with the comment text
+    const commentThread = new Thread({
+      text: commentText,
+      author: userId,
+      parentId: threadId,
+    })
+
+    // Save the new thread
+    const savedCommentThread = await commentThread.save()
+
+    // Update the original thread to include the new comment
+    originalThread.children.push(savedCommentThread._id)
+
+    // Save the original thread
+    await originalThread.save();
+
+    revalidatePath(path)
+  } catch (error: any) {
+    throw new Error(`Error adding comment to thread: ${error.message}`)
+  }
+
+
+}
