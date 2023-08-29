0e7eab23e3cea8a7af69738b832ff28c3e18f4f7
Twan Hopmans <twanjhopmans@hotmail.com>
2023-08-29 10:40:04 +0200

    ignored ts errors

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    ignoredBuildErrors: true,
+    ignoreBuildErrors: true,
   },
   experimental: {
     serverActions: true,
