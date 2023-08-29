757ee33cd2db52f5d465b378439fbbc1f2841a07
Twan Hopmans <twanjhopmans@hotmail.com>
2023-08-29 10:55:09 +0200

    ignore build error

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
+  typescript: {
+    ignoreBuildErrors: true,
   },
   experimental: {
     serverActions: true,
