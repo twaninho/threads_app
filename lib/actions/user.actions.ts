34b0c76ec85ec50afd08d3c5d43fffd100623e77
Twan Hopmans <twanjhopmans@hotmail.com>
2023-08-29 09:35:26 +0200

    activity page

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -138,3 +138,32 @@ export async function fetchUsers({
         throw new Error (`Failed to fetch users: ${error.message}`)
     }
 }
+
+export async function getActivity(userId: string) {
+    try {
+        connectToDB()
+
+        // find all threads created by the user
+        const userThreads = await Thread.find({ author: userId})
+
+        // collect all the child thread ids (replies) from each thread
+        const childThreadIds = userThreads.reduce((acc, userThread) => {
+            return acc.concat(userThread.children)
+        }, [])
+
+        // find all the threads that the user has replied to
+        const replies = await Thread.find({
+            _id: { $in: childThreadIds },
+            author: { $ne: userId}
+        }).populate({
+            path: 'author',
+            model: User,
+            select: 'name image _id'
+        })
+
+        return replies 
+
+    } catch (error: any) {
+        throw new Error (`Failed to fetch activity: ${error.message}`)
+    }
+}
